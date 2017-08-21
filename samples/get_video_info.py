f2acbebe18b9953d0608068a097ca5149a56898b
diegoami <diego.amicabile@gmail.com>
2017-08-21 19:43:17 +0200

    Made to deal with languages

--- samples/get_video_info.py
+++ samples/get_video_info.py
@@ -9,10 +9,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--videoId')
 
-
     args = parser.parse_args()
     amara = Amara(amara_headers)
-    amaraVideo = amara.retrieve_video(args.videoId)
-    if (amaraVideo):
-        subtitles = amaraVideo.get_best_subtitles()
-        print(subtitles)
+    amara_video = amara.retrieve_video(args.videoId)
+    if (amara_video ):
+        video_info = amara_video .get_video_info()
+        print(video_info )
+        languages = amara_video .get_languages()
+        print(languages)
